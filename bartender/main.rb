c594ef90b72f0540dea64f00e88e843e5657284d
sakalmon <sakalathmon@gmail.com>
2022-12-13 20:41:15 +1000

    Basic HTML structure

--- bartender/main.rb
+++ bartender/main.rb
@@ -1,7 +1,9 @@
-     
 require 'sinatra'
+require 'httparty'
 
 get '/' do
+  data = HTTParty.get('http://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita').parsed_response
+  p data['drinks'][0]
   erb :index
 end
 
