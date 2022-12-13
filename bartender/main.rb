05562d83f4b4c423380fd8310bbea87491db5e71
sakalmon <sakalathmon@gmail.com>
2022-12-13 21:47:29 +1000

    Basic header

--- bartender/main.rb
+++ bartender/main.rb
@@ -2,8 +2,8 @@ require 'sinatra'
 require 'httparty'
 
 get '/' do
-  data = HTTParty.get('http://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita').parsed_response
-  p data['drinks'][0]
+  # data = HTTParty.get('http://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita').parsed_response
+  # p data['drinks'][0]
   erb :index
 end
 
