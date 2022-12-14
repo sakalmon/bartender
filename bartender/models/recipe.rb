4352dc52ca2896c4b94c510bcc7949e0e692fbab
sakalmon <sakalathmon@gmail.com>
2022-12-14 18:56:02 +1000

    Search feature

--- bartender/models/recipe.rb
+++ bartender/models/recipe.rb
@@ -60,3 +60,59 @@ end
 def delete_recipe(id)
   run_sql("DELETE FROM recipes WHERE id = $1", [id])
 end
+
+def search_recipe(query)
+  data = HTTParty.get("http://www.thecocktaildb.com/api/json/v1/#{ENV['API_KEY']}/search.php?s=#{query}").parsed_response
+
+  drinks = []
+
+  drinks_data = data['drinks']
+
+  drinks_data.each do |drink_data|
+    ingredients = {}
+    ingredients_amt = {}
+
+    name = drink_data['strDrink']
+    instructions = drink_data['strInstructions']
+    image_url = drink_data['strDrinkThumb']
+    
+    drink_data.each_pair do |key, val|
+      if key.include?('Ingredient')
+        ingredients[key] = val
+      end
+      if key.include?('Measure')
+        ingredients_amt[key] = val
+      end
+    end
+
+    ingredients_json = JSON.generate(ingredients)
+    ingredients_amt_json = JSON.generate(ingredients_amt)
+
+    drink = {
+      'name' => name,
+      'ingredients' => ingredients_json,
+      'ingredients_amt' => ingredients_amt_json,
+      'instructions' => instructions,
+      'image_url' => image_url
+    }
+
+    drinks.push(drink)
+  end
+
+  recipes = drinks
+
+  parsed_recipes = []
+
+  recipes.each do |recipe|
+    drink = {}
+    recipe.each_pair do |key, val|
+      if key == 'ingredients' || key == 'ingredients_amt'
+        drink[key] = JSON.parse val
+      else
+        drink[key] = val
+      end
+    end
+    parsed_recipes.push drink
+  end
+  parsed_recipes
+end
